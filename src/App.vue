ad0435b16df134334cc7d8be97f4f064d1c976eb
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-10 12:45:52 +0100

    import componenti vue, axios e store.js

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,30 @@
-<script setup>
+<script>
+// importo componenti vue
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
 
+// importo axios
+import axios from 'axios';
+
+// importo file store.js
+import { store } from './store'
+
+export default {
+  components: {
+    AppHeader,
+    AppMain
+  },
+  data() {
+    return {
+      store
+    }
+  }
+}
 </script>
 
-<template></template>
+<template>
+  <AppHeader />
+  <AppMain />
+</template>
 
-<style scoped></style>
+<style lang="scss"></style>
