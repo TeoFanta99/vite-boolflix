2d656d122653456d659abe45ea54af20e97564fb
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-10 13:11:40 +0100

    creazione file, aggiunto html e css provvisori

---
+++ src/components/AppCard.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: "AppCard"
+}
+</script>
+
+<template>
+    <div class="card">
+        CARD
+    </div>
+</template>
+
+<style lang="scss">
+.card {
+    min-height: 100px;
+    width: calc((100% / 5) - 5px);
+    background-color: orange;
+
+    img {
+        width: 100%;
+    }
+}
+</style>
