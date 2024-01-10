4223d67e395981055e4bc184bd794d8265c473a5
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-10 12:50:52 +0100

    inclusione file scss

--- src/App.vue
+++ src/App.vue
@@ -27,4 +27,7 @@ export default {
   <AppMain />
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+@use './styles/general.scss' as *;
+@use './styles/partials/variables' as *;
+</style>
