31fde892856e53ae0c29dfc823fe94ed5671b5c7
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-10 14:18:55 +0100

    add apiURL + array filmList

--- src/store.js
+++ src/store.js
@@ -1,5 +1,7 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
+    apiURL: 'https://api.themoviedb.org/3/discover/movie?api_key=4236ad0339f4cfaee31f8ca61b211d01',
+    filmList: [],
 
 })
