ea034ed1e8b6d926e6b7f334e7b9f8ce48d7540d
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-10 17:56:13 +0100

    creazione componente + template

---
+++ src/components/AppSearch.vue
@@ -0,0 +1,36 @@
+<script>
+import { store } from '../store'
+
+export default {
+    name: "AppSearch",
+    data() {
+        return {
+            store
+        }
+    },
+    methods: {
+        saluta() {
+            this.$emit('confirm');
+            console.log("emesso??");
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="search-container">
+        <input v-model.trim="store.filmParam" type="text" placeholder="Cerca" class="me-2">
+        <button type="button" class="me-2" @click.prevent="saluta">SEARCH</button>
+    </div>
+</template>
+
+<style lang="scss">
+header {
+    min-height: 80px;
+    width: 100%;
+    background-color: lightblue;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+</style>
