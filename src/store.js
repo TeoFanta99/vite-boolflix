0c99f1a56673aef5229b41a08b19d78055ab36b7
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-11 13:21:51 +0100

    add apiurl per serie tv + array + param

--- src/store.js
+++ src/store.js
@@ -3,5 +3,9 @@ import { reactive } from 'vue'
 export const store = reactive({
     apiURL: 'https://api.themoviedb.org/3/search/movie?language=it-IT&api_key=4236ad0339f4cfaee31f8ca61b211d01',
     filmList: [],
-    filmParam: ""
+    filmParam: "",
+
+    tvSeriesApiURL: 'https://api.themoviedb.org/3/search/tv?language=it-IT&api_key=4236ad0339f4cfaee31f8ca61b211d01',
+    tvSeriesList: [],
+    tvSeriesParam: "",
 })
