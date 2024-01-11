8feb4cfbab4d611498d1c2b9e7bb5765e7cbb7d6
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-11 10:42:40 +0100

    add metodo searchFilm

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,4 +1,5 @@
 <script>
+import axios from 'axios';
 import { store } from '../store'
 import AppSearch from './AppSearch.vue'
 
@@ -13,17 +14,45 @@ export default {
         AppSearch
     },
     methods: {
-        saluta() {
-            console.log("evento ricevuto");
+        searchFilm() {
+            // quello che scrive l'utente nell'input ora deve apparire in console
+            console.log(store.filmParam);
+
+            let myURL = store.apiURL;
+            console.log(myURL);
+
+            // quello che scrive l'utente nell'input va aggiunto al link API. Inizio col verificare se l'utente ha fatto una ricerca
+            if (store.filmParam !== "") {
+
+                // ora verifico se la richiesta API (il link) contiene già dei parametri. Se li contiene, aggiungo '&':
+                if (myURL.includes('?')) {
+                    myURL += `&query=${store.filmParam}`
+                }
+                // Se non contiene di parametri aggiungo '?'
+                else {
+                    myURL += `?query=${store.filmParam}`
+                }
+                console.log(myURL);
+            }
+
+            // ora faccio partire la chiamata axios per cercare i film con i parametri aggiunti dall'utente
+            axios
+                .get(myURL)
+                .then((res => {
+                    console.log(res.data);
+                }))
+                .catch((err) => {
+                    console.log("errori", err);
+                })
         }
-    }
+    },
 }
 </script>
 
 <template>
     <header>
         <div class="logo-container"></div>
-        <AppSearch @confirm="saluta" />
+        <AppSearch @ricerca="searchFilm" />
     </header>
 </template>
 
