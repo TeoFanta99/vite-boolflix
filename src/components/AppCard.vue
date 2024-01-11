505b2ecfcd9aa1bd619b8d1272cc60e05f2062f5
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-11 11:12:57 +0100

    rimosso tutto

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -3,32 +3,9 @@ import { store } from '../store'
 
 export default {
     name: "AppCard",
-    props: {
-        info: Object
-    },
-    data() {
-        return {
-            store
-        }
-    }
 }
 </script>
 
-<template>
-    <!-- elemento da rendere dinamico -->
-    <div class="film-card">
-        CARD
-    </div>
-</template>
+<template></template>
 
-<style lang="scss">
-.row>.film-card {
-    min-height: 400px;
-    width: calc((100% / 3) - 5px);
-    background-color: orange;
-
-    img {
-        width: 100%;
-    }
-}
-</style>
+<style lang="scss"></style>
