bd99abf3f0618508ada25cd3e7a2952c63077d18
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-11 19:08:07 +0100

    added <br> e css per spazi tra le scritte + add overview

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -23,7 +23,8 @@ export default {
 
         // metodo che restituisce l'immagine di copertina
         getBackgroundImg: function (img) {
-            return new URL(`https://image.tmdb.org/t/p/w342/${img}`)
+            if (img)
+                return new URL(`https://image.tmdb.org/t/p/w342/${img}`)
         },
 
         // metodo per sostituire il voto del film con il numero di stelle piene corrispondenti, arrotondando per eccesso i numeri decimali
@@ -46,16 +47,20 @@ export default {
             <div class="row">
                 <h1>FILM</h1>
                 <!-- elemento da iterare (film)-->
-                <!-- front -->
                 <div v-for="(film, i) in store.filmList" :key="i" :info="film" class="film-card">
                     <img class="front-card" :src=getBackgroundImg(film.poster_path) alt="immagine di copertina non trovata">
                     <div class="back-card">
                         <span><b>Titolo</b>: {{ film.title }}</span>
+                        <br>
                         <span><b>Titolo originale</b>: {{ film.original_title }}</span>
+                        <br>
                         <span><b>Lingua originale</b>:
                             <img :src="getFlagImageUrl(film.original_language)" alt="bandiera" class="flag-language">
                         </span>
+                        <br>
                         <span class="stars"><b>Valutazione:</b> {{ getStars(film.vote_average) }}</span>
+                        <br>
+                        <span><b>Overview</b>: {{ film.overview }}</span>
                     </div>
 
 
@@ -65,13 +70,23 @@ export default {
                 <h1>SERIE TV</h1>
                 <!-- elemento da iterare (serie tv) -->
                 <div v-for="(tvserie, i) in store.tvSeriesList" :key="i" :info="film" class="film-card">
-                    <span><b>Titolo</b>: {{ tvserie.name }}</span>
-                    <span><b>Titolo originale</b>: {{ tvserie.original_name }}</span>
-                    <span><b>Lingua originale</b>:
-                        <img :src="getFlagImageUrl(tvserie.original_language)" alt="bandiera" class="flag-language">
-                    </span>
-                    <span class="stars"><b>Valutazione:</b> {{ getStars(tvserie.vote_average) }}</span>
-                    <img :src=getBackgroundImg(tvserie.poster_path) alt="immagine di copertina non trovata">
+                    <img class="front-card" :src=getBackgroundImg(tvserie.poster_path)
+                        alt="immagine di copertina non trovata">
+                    <div class="back-card">
+                        <span><b>Titolo</b>: {{ tvserie.name }}</span>
+
+                        <span><b>Titolo originale</b>: {{ tvserie.original_name }}</span>
+                        <br>
+                        <span><b>Lingua originale</b>:
+                            <img :src="getFlagImageUrl(tvserie.original_language)" alt="bandiera" class="flag-language">
+                        </span>
+                        <br>
+                        <span class="stars"><b>Valutazione:</b> {{ getStars(tvserie.vote_average) }}</span>
+                        <br>
+                        <span><b>Overview</b>: {{ tvserie.overview }}</span>
+                    </div>
+
+
                 </div>
             </div>
         </div>
@@ -102,7 +117,14 @@ main {
         width: auto;
         max-height: 20px;
     }
+
+    span {
+        margin-top: 2px;
+        display: block;
+    }
 }
+
+
 
 .front-card,
 .back-card {
@@ -113,7 +135,6 @@ main {
     bottom: 0;
     display: flex;
     flex-direction: column;
-    padding: 10px;
     color: white;
     transition: transform 0.5s ease;
 }
@@ -123,6 +144,12 @@ main {
     width: 100%;
     height: 100%;
     object-fit: cover;
+    padding: 5px;
+}
+
+.back-card {
+    font-size: 12px;
+    padding: 10px;
 }
 
 .film-card:hover .front-card {
@@ -131,6 +158,7 @@ main {
 
 .film-card:hover .back-card {
     transform: translateY(0);
+    display: block;
 }
 
 .stars {
