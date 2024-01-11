7b14ef8d93874a9e8425801ca61da096a56e4e59
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-11 11:12:24 +0100

    rem AppCard e unione ad AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,11 @@
 <script>
-import AppCard from './AppCard.vue'
+// import AppCard from './AppCard.vue'
 import { store } from '../store'
 
 export default {
     name: "AppMain",
     components: {
-        AppCard
+        // AppCard
     },
     data() {
         return {
@@ -19,7 +19,12 @@ export default {
     <div class="container">
         <div class="row">
             <!-- elemento da iterare -->
-            <AppCard v-for="(film, i) in filmList" :key="i" :info="film" />
+            <div v-for="(film, i) in filmList" :key="i" :info="film" class="film-card">
+                <h2>{{ res.data.results[i].title }}</h2>
+                <span>{{ res.data.results[i].original_title }}</span>
+                <span>{{ res.data.results[i].original_language }}</span>
+                <span>{{ res.data.results[i].vote_average }}</span>
+            </div>
         </div>
     </div>
 </template>
@@ -28,5 +33,15 @@ export default {
 .row {
     margin: 30px auto;
     gap: 5px
+}
+
+.row>.film-card {
+    min-height: 400px;
+    width: calc((100% / 3) - 5px);
+    background-color: orange;
+
+    img {
+        width: 100%;
+    }
 }
 </style>
